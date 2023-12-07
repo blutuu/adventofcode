6345e494b64890467beeb28cd921b81f7e37d3e8
Marlin <blutuu1@gmail.com>
2023-12-07 06:32:57 -0500

    d1p2

--- src/aoc.js
+++ src/aoc.js
@@ -5,38 +5,64 @@ const readline = require("readline");
 class Aoc {
   constructor() {
     this.input = null;
+    this.firstDigit = null;
+    this.secondDigit = null;
+    this.numberstrings = [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+    ];
   }
 
   // Day 1 - Part 1 Calibration Values
   getCalibrationValue(input) {
     let j = input.length - 1;
-    let firstDigit = "",
-      secondDigit = "";
 
-    for (let i = 0; i < input.length; i++) {
-      if (Number.isInteger(parseInt(input[i])) && firstDigit === "") {
-        firstDigit = input[i];
+    this.checkForNumberString();
+    console.log(this.firstDigit + "" + this.secondDigit);
+
+    return parseInt(this.firstDigit + "" + this.secondDigit);
+  }
+
+  checkForNumberString() {
+    let regex =
+      /(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9)/gi;
+    let match;
+    this.firstDigit = null;
+
+    while ((match = regex.exec(this.input)) !== null) {
+      if (this.firstDigit === null) {
+        this.firstDigit = parseInt(match[0])
+          ? match[0]
+          : this.numberstrings.indexOf(match[0]) + 1;
+        this.input = "three";
       }
-      if (Number.isInteger(parseInt(input[j])) && secondDigit === "") {
-        secondDigit = input[j];
-      }
-      j--;
+      // console.log(`Found ${match[0]} at index ${match.index}`);
+      this.secondDigit = parseInt(match[0])
+        ? match[0]
+        : this.numberstrings.indexOf(match[0]) + 1;
     }
-
-    return parseInt(firstDigit + secondDigit);
+    // console.log();
   }
 }
 
 // create a new instance of the class
 const aoc = new Aoc();
-aoc.getCalibrationValue("mdmvbhqjt5rkfpcnfvzhkkfbjvh8three9");
-
+let total = 0;
+let index = 0;
 let lineReader = readline.createInterface({
-  input: fs.createReadStream("inputs/day1.txt"),
+  input: fs.createReadStream("./src/inputs/day1.txt"),
 });
 
-let total = 0;
 lineReader.on("line", function (line) {
+  aoc.input = line;
+  process.stdout.write(++index + ": ");
   total += aoc.getCalibrationValue(line);
 });
 lineReader.on("close", function () {
